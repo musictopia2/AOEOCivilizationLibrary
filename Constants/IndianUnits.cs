70f34ba47e5ef4b1bb819df829668dcfe7a17e69
Andy Walter <musictopia2@hotmail.com>
2024-09-29 08:02:00 -0700

    Added the indian units so there is now intellisense for indians as well.

---
+++ Constants/IndianUnits.cs
@@ -0,0 +1,38 @@
+﻿namespace AOEOCivilizationLibrary.Constants;
+public static class IndianUnits
+{
+    public const string Farm = "In_Bldg_Farm";
+    public const string Storehouse = "In_Bldg_StoreHouse";
+    public const string Monastery = "In_Bldg_Monastery";
+    public const string Market = "In_Bldg_Market";
+    public const string Shrine = "In_Bldg_Shrine";
+    public const string GuardTower = "In_Bldg_GuardTower";
+    public const string Wall = "In_WallConnector";
+    public const string Gate = "In_WallGate";
+    public const string Dock = "In_Bldg_Dock";
+    public const string WatchPost = "In_Bldg_LookoutTower";
+    public const string Wonder = "In_Bldg_Wonder";
+    public const string Villager = "In_Civ_Villager";
+    public const string GathererElephant = "In_Civ_GathererElephant";
+    public const string Guru = "In_Spc_Guru";
+    public const string Caravan = "In_Civ_Caravan";
+    public const string FishingBoat = "In_Civ_FishingBoat";
+    public const string MerchantTransport = "In_Civ_UtilityBoat";
+    public const string Vimana = "In_Shp_Vimana";
+    public const string Fireship = "In_Shp_FireShip";
+    public const string Spearman = "In_Inf_Spearman";
+    public const string GadaWarrior = "In_Inf_GadaWarrior";
+    public const string Bowman = "In_Arc_Bowman";
+    public const string Ranger = "In_Arc_Ranger";
+    public const string Turanga = "In_Cav_Turanga";
+    public const string BladeChariot = "In_Arc_BladeChariot";
+    public const string WarElephant = "In_Cav_WarElephant";
+    public const string ElephantArcher = "In_Arc_ElephantArcher";
+    public const string Juggernaut = "In_Cav_Juggernaut";
+    public const string Ram = "In_Sie_Ram";
+    public const string Palintonon = "In_Sie_Palintonon";
+    public const string Scout = "In_Inf_Scout";
+    public const string In_SacredCow = "Sacred Cow";
+    public const string ClubMonkey = "In_Inf_Monkey1";
+    public const string FireMonkey = "In_Arc_Monkey2";
+}
