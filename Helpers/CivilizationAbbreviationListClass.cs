45b6afb49e784f12b1c86888b7ca9407e9188fd8
Andy Walter <musictopia2@hotmail.com>
2026-05-15 14:14:44 -0700

    New class where i can get a list of all civilization abbreviations easily without interfaces.

---
+++ Helpers/CivilizationAbbreviationListClass.cs
@@ -0,0 +1,15 @@
+﻿namespace AOEOCivilizationLibrary.Helpers;
+public static class CivilizationAbbreviationListClass
+{
+    public static BasicList<string> GetAllCivilizations =>
+        [
+        AllCivAbbreviations.Indian,
+        AllCivAbbreviations.Roman,
+        AllCivAbbreviations.Greek,
+        AllCivAbbreviations.Egyptian,
+        AllCivAbbreviations.Norse,
+        AllCivAbbreviations.Persian,
+        AllCivAbbreviations.Babylonian,
+        AllCivAbbreviations.Celt
+        ];
+}
